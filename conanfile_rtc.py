5f89a7efaa69a496aa14f001363209eec5ab8fba
Christian Venegas <cvenegas@esri.com>
2020-11-05 13:48:18 -0800

    build: Add a conanfile to package artifacts (#6)
    Issue-number: https://devtopia.esri.com/runtime/devops/issues/693

---
+++ conanfile_rtc.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+
+class CppcheckConan(ConanFile):
+    name = "cppcheck"
+    version = "1.90"
+    url = "https://github.com/Esri/cppcheck/tree/runtimecore"
+    license = "https://github.com/Esri/cppcheck/blob/runtimecore/COPYING"
+    description = "Static analysis of C/C++ code"
+
+    # Use the OS default to pick the correct executables
+    settings = "os"
+
+    def package(self):
+        base = self.source_folder + "/"
+
+        # tools
+        tools = "tools/cppcheck"
+        self.copy("*", src=base + "../../" + tools, dst=tools)
